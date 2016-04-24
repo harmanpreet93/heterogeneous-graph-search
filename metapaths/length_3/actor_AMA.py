480b40ad09012fc39e826b0c064b59684282df81
Harmanpreet Singh <cs12b1017@iith.ac.in>
2016-04-24 16:51:31 +0530

    similarity score calucated for length 3 metapaths

--- metapaths/length_3/actor_AMA.py
+++ metapaths/length_3/actor_AMA.py
@@ -1,15 +1,5 @@
 #!usr/bin/python
 
-'''
-Data from modified_actors.txt is uploaded to nei4j using following steps:
-	- For each actor, create a node with label 'actor' and property 'name'
-	- For each actor, find all the movies that the actor has acted in and 
-		create a relationship between the two
-
-	Structure of modified_actors.txt file:
-	- actor,<movies actor acted in>
-	  For eg: James (II) Coyne,Man-Thing (2005),'Missing Sock (2004)
-'''
 
 from py2neo import authenticate,Graph
 from py2neo import Node, Relationship
@@ -43,41 +33,51 @@ def execute_query(query):
 
 if __name__ == '__main__':
 
-	actor_input = raw_input('Enter the actor for whom to find similarity: ')
-	top_K = raw_input('Enter the actor for whom to find similarity: ')
+	actor_input = str(raw_input('Enter the actor: '))
+	top_K = int(raw_input('Enter k for similarity: '))
 
 
 	graph = connect_graph()
 	query_xx = "MATCH (a:actor {name:'"+str(actor_input)+"'})-[ACTED_IN]->(m) MATCH (m)<-[r:ACTED_IN]-(a) RETURN a as actor,count(a.name) as count"
 	xx = execute_query(query_xx)
 	
-	for result in xx:
-		xx_value = int(result.count)
-		break
+	if len(xx) == 0:
+		xx_value = 0
+	else:	
+		for result in xx:
+			xx_value = int(result.count)
+			break
+	print "xx: ",xx_value
 
 	query_xy = "MATCH (a:actor {name:'"+str(actor_input)+"'})-[r1:ACTED_IN]->(m1:movie)<-[r2:ACTED_IN]-(b:actor) RETURN b as actor,count(b.name) as count"
 	xy = execute_query(query_xy)
 
+
 	hashmap = {}
 	for result in xy:
-		hashmap.setdefault(result.actor,[]) 
-		hashmap[result.actor].append(result.count)
+		hashmap.setdefault(result.actor['name'],[]) 
+		hashmap[result.actor['name']].append(result.count)
 
 	query_yy = "MATCH (a:actor {name:'"+str(actor_input)+"'})-[r1:ACTED_IN]->(m1:movie)<-[r2:ACTED_IN]-(b:actor) MATCH (b)-[r3:ACTED_IN]->(m) MATCH (m)<-[r4:ACTED_IN]-(b) RETURN b as actor,count(b.name) as count"
 	yy = execute_query(query_yy)
 
 	for result in yy:
-		hashmap[result.actor].append(result.count)
+		hashmap[result.actor['name']].append(result.count)
 
-	# print hashmap
+	output_file = open("../../sample_outputs/length_3/" +actor_input+"_output_AMA.txt", "w")
 
-	for key in hashmap:
-		xy_value = int(hashmap[key][0])
-		yy_value = int(hashmap[key][1])
-		score = 2 * xy_value / (xx_value+yy_value)
-		hashmap[key].append(score)
+	for key, value in hashmap.iteritems():
+		xy_value = int(value[0])
+		yy_value = int(value[1])
+		score = (2*xy_value)/float((xx_value+yy_value))
+		value.append(score)
 
 	sorted_hashmap = sorted(hashmap.items(), key=lambda e: e[1][2])
-	for key in sorted_hashmap:
-		print key, sorted_hashmap[key][2]
 
+	for i in range(len(sorted_hashmap)-1,len(sorted_hashmap)-top_K-1,-1):
+		print sorted_hashmap[i][0],sorted_hashmap[i][1][2]
+
+	for i in sorted_hashmap:
+		output_file.write(str(i) + '\n')
+
+	output_file.close()
